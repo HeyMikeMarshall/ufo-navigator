f44658f3cc38e621e5e600522352991dc4ed52c9
HeyMikeMarshall <mjmarshall@gmail.com>
2019-06-18 12:36:01 -0400

    level 1 complete

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,21 @@
-// from data.js
 var tableData = data;
+var filter = d3.select("#filter-btn");
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+filter.on("click", function() {
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    var filterdata = tableData.filter(data => data.datetime === inputValue);
+    filterdata.forEach((uforeport) => {
+        var row = tbody.append("tr");
+        Object.entries(uforeport).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+  });
+
+
+
